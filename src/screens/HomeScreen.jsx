9c88e90a6795e2309a3802844547bb1000f48506
Ota <q.tonellato@free.fr>
2025-05-22 14:01:22 +0200

    feat: add callout on map

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Text, Button } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 
 export default function App() {
   return (
@@ -8,12 +8,25 @@ export default function App() {
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: 48.8588443,
-          longitude: 2.2943506,
+          latitude: 47.216671,
+          longitude: -1.55,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <Marker
+          coordinate={{ latitude: 47.216671, longitude: -1.55 }}
+          title="Mon Marqueur"
+          description="Ceci est un exemple de callout personnalisé"
+        >
+          <Callout tooltip={true}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Restaurant italien</Text>
+              {/*<Button>Voir la carte du restaurant</Button>*/}
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -25,5 +38,15 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  calloutContainer: {
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  calloutText: {
+    fontSize: 16,
   },
 });
